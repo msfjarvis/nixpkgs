bf6ab9f3274eae7e06da1c164e83a3614bda5b4f
Harsh Shandilya <me@msfjarvis.dev>
2024-06-17 00:41:34 +0530

    kondo: install shell completion

--- pkgs/applications/misc/kondo/default.nix
+++ pkgs/applications/misc/kondo/default.nix
@@ -1,4 +1,4 @@
-{ lib, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub, installShellFiles }:
 
 rustPlatform.buildRustPackage rec {
   pname = "kondo";
@@ -12,6 +12,22 @@ rustPlatform.buildRustPackage rec {
   };
 
   cargoHash = "sha256-WF4GHj/5VYrTUh1E3t29zbpSLjJ6g7RWVpLYqg9msZg=";
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  postBuild = ''
+    mkdir -p completions
+    for shell in bash fish zsh; do
+        cargo run -- --completions $shell > completions/kondo.$shell
+    done
+  '';
+
+  postInstall = ''
+    installShellCompletion --cmd kondo \
+      --bash completions/kondo.bash \
+      --fish completions/kondo.fish \
+      --zsh completions/kondo.zsh
+  '';
 
   meta = with lib; {
     description = "Save disk space by cleaning unneeded files from software projects";
