a79df8aa131f3e89e5b01efe841fe06a4e69b9c1
Harsh Shandilya <me@msfjarvis.dev>
2024-07-20 13:58:26 +0530

    python.myjdapi: init at 1.1.10

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11359,6 +11359,8 @@ self: super: with self; {
 
   myhome = callPackage ../development/python-modules/myhome { };
 
+  myjdapi = callPackage ../development/python-modules/myjdapi { };
+
   myjwt = callPackage ../development/python-modules/myjwt { };
 
   mypy = callPackage ../development/python-modules/mypy { };
