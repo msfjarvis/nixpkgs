93d09e04d7158844c9778eac0587d742ef192366
Harsh Shandilya <me@msfjarvis.dev>
2024-07-20 13:55:59 +0530

    python3Packages.mediafire: init at 0.6.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10661,6 +10661,8 @@ self: super: with self; {
 
   mediafile = callPackage ../development/python-modules/mediafile { };
 
+  mediafire = callPackage ../development/python-modules/mediafire { };
+
   mediapy = callPackage ../development/python-modules/mediapy { };
 
   mediawiki-langcodes = callPackage ../development/python-modules/mediawiki-langcodes { };
