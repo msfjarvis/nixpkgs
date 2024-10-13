53a2e4893de68732f1ed9cdbcee7a4425593b91d
Harsh Shandilya <me@msfjarvis.dev>
2024-10-13 16:58:31 +0530

    attic: add patch to enable derivation filtering

--- pkgs/by-name/at/attic-client/package.nix
+++ pkgs/by-name/at/attic-client/package.nix
@@ -26,6 +26,8 @@ rustPlatform.buildRustPackage {
     hash = "sha256-CvaKOUq8G10sghKpZhEB2UYjJoWhEkrDFggDgi7piUI=";
   };
 
+  patches = [ ./exclude-filter.patch ];
+
   nativeBuildInputs = [
     pkg-config
     installShellFiles
