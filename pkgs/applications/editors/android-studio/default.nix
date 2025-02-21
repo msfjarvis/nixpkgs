f325251ee444f268aeb7e2da3a2b2a8d79d2069d
Harsh Shandilya <me@msfjarvis.dev>
2025-02-21 21:00:04 +0530

    androidStudioPackages.beta: 2024.3.1.10 -> 2024.3.1.12

--- pkgs/applications/editors/android-studio/default.nix
+++ pkgs/applications/editors/android-studio/default.nix
@@ -13,8 +13,8 @@ let
     sha256Hash = "sha256-c9t8GQw2azYIf4vyiKSolnbGTUeq25rYxSgBDei7I0Q=";
   };
   betaVersion = {
-    version = "2024.3.1.10"; # "Android Studio Meerkat | 2024.3.1 Beta 1"
-    sha256Hash = "sha256-UxxofUCJGhQTLMwHGaSdNDqWnjkpRVwm2oqLLp3jR8E=";
+    version = "2024.3.1.12"; # "Android Studio Meerkat | 2024.3.1 RC 2"
+    sha256Hash = "sha256-gi7kaB4cPnnYqHJcoeKdQ6+InIzW1kaX5kBuKR48u+Q=";
   };
   latestVersion = {
     version = "2024.3.2.2"; # "Android Studio Meerkat Feature Drop | 2024.3.2 Canary 2"
